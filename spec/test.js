a27d4ee84209f2ae78e4dfe4bfe6ce7660133e4b
xsimov <xsimov@gmail.com>
2014-07-12 19:48:23 +0200

    First rules applied

--- spec/test.js
+++ spec/test.js
@@ -3,8 +3,45 @@
 
 (function () {
   describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-      expect(true).toBe(true);
+
+    it("has a SellIn value", function(){
+
+    	var potato = new Item("Potato", 20, 10);
+    	items.push(potato);
+    	var fish = new Item("Fish", 20, 10);
+    	items.push(fish);
+
+    	for (var i = 0; i < items.length; i++) {
+    		expect(items[i].sellIn).not.toBe(null);
+    	};
     });
+
+    it("has a quality value", function(){
+
+    	var potato = new Item("Potato", 20, 10);
+    	items.push(potato);
+       	var fish = new Item("Fish", 20, 10);
+    	items.push(fish);
+
+    	for (var i = 0; i < items.length; i++) {
+    		expect(items[i].quality).not.toBe(null);
+    	};
+    });
+
+    it("system lowers sellIn and quality value every day", function(){
+
+    	var potato = new Item("Potato", 20, 10);
+    	items.push(potato);
+    	var fish = new Item("Fish", 48, 8);
+    	items.push(fish);
+
+    	for (var i = 0; i < items.length; i++) {
+    		expect(items[i].sellIn).toBe(items[i].sellIn);
+    		expect(items[i].quality).toBe(items[i].quality);
+    	};
+
+    });
+
+
   });
 })();
